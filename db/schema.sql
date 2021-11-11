803a4e642bb62a6672c5e6ac3fe4a2a48da7056a
lills1 <lilly.stephenson4@gmail.com>
2021-11-11 11:56:39 +1100

    added in server.js adn setup database connection, modified some names in schema file to make it more readable

--- db/schema.sql
+++ db/schema.sql
@@ -5,20 +5,20 @@ USE employee_db;
 
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- dept_name VARCHAR(30) NOT NULL
+ department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE employees_role (
+CREATE TABLE employee_role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(40) NOT NULL,
   salary DECIMAL,
-  dept_id INT,
-  FOREIGN KEY (dept_id)
-  REFERENCES dept(id)
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
   ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -29,5 +29,5 @@ CREATE TABLE employees (
   manager_id INT,
   FOREIGN KEY (employee_id)
   REFERENCES employee(id)
-  ON DELETE SET NULL,
+  ON DELETE SET NULL
 );
