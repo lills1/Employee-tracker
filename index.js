df8e1aefbe787ba223e5150dcecc798e009d0006
lills1 <lilly.stephenson4@gmail.com>
2021-11-12 12:45:10 +1100

    created functions to run in cli

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-
+const mysql = require("mysql2");
+const table = require("console.table");
+const db = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "FizzBuzz1",
+    database: "employee_db"
+})
+db.connect(function (err) {
+    if (err) throw err
+    createList()
+})
 function createList() {
     inquirer
         .prompt([
@@ -13,7 +25,72 @@ function createList() {
         ])
 
         .then((answers) => {
-            console.log(answers);
+            // console.log(answers);
+            if (answers.teamMembers === "view all departments") {
+                viewDepartments();
+            }
+            if (answers.teamMembers === "view all roles") {
+                viewRoles();
+            }
+            if (answers.teamMembers === "view all employees") {
+                viewEmployees();
+            }
+            if (answers.teamMembers === "add a department") {
+                addDepartment();
+            }
+            if (answers.teamMembers === "add a role") {
+                addRole();
+            }
+            if (answers.teamMembers === "add an employee") {
+                addEmployee();
+            }
+            if (answers.teamMembers === "update an employee role") {
+                updateRole();
+            }
         });
 }
-createList();
+function viewDepartments() {
+    db.query("select * from department", function (err, data) {
+        console.table(data)
+        createList();
+    })
+}
+function viewRoles() {
+    db.query("select* from role", function (err, data) {
+        console.table(data)
+        createList();
+    })
+}
+function viewEmployees() {
+    db.query("select * from employee", function (err, data) {
+        console.table(data)
+        createList();
+    })
+}
+function addDepartment() {
+    inquirer
+        .prompt(
+            [
+                {
+                    type: "input",
+                    name: "departmentName",
+                    message: "What is your department you want to add?"
+                }
+            ]
+        )
+        .then(function (answers) {
+            db.query("insert into department(name) values(?)", [answers.departmentName],   function (err, data) {
+                console.log("Your department has been added")
+                createList();
+            })
+        })
+}
+function addRole() {
+
+}
+function addEmployee() {
+
+}
+function updateRole() {
+
+}
